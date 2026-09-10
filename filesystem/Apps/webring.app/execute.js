85edd1c3dc72160813933d7b6631cd3d4815fc75
Lohrrrr <126462870+lohrrrr@users.noreply.github.com>
2026-09-10 21:32:11 +0000

    Final test before emailing webring's webmaster

--- filesystem/Apps/webring.app/execute.js
+++ filesystem/Apps/webring.app/execute.js
@@ -1,18 +1,1 @@
-function loadScriptSequential(src) {
-  return new Promise(function(resolve, reject) {
-    var s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.src = src;
-    s.onload = resolve;
-    s.onerror = reject;
-    document.body.appendChild(s);
-  });
-}
-
-loadScriptSequential('https://fabstarotcorner.neocities.org/webring/onionring-variables.js')
-  .then(function() {
-    return loadScriptSequential('https://fabstarotcorner.neocities.org/webring/onionring-widget.js');
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
+// I HATE THIS, REALY, I BROKE COPY&PASTE WIDGET
